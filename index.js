ed25cdedbad91eff8be503a97ffa72dca7bb921b
Brandon Weygant <bobbydigital108@gmail.com>
2019-06-12 14:04:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(height){
+  let response = '';
+  if (height <= 400) {
+    response = 'This one is on me!';
+  } else if (height > 2000 && height <= 2500) {
+    response = 'I will gladly take your thirty bucks.';
+  } else if (height > 2500) {
+    response = 'No can do.';
+  }
+  return response;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let response = '';
+  city === 'NYC' ? response = 'Ok, sounds good.' : response = 'No go.';
+  return response;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let response = '';
+  if (tip === 'generous' ) {
+    response = 'Thank you so much.';
+  } else if (tip === 'not as generous') {
+    response = 'Thank you.';
+  } else {
+    response = 'Bye.';
+  }
+  return response;
 }
